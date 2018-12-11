c2ff62a0e165473dc8f6117b6129d8860b585f75
Tranelle Maner <tranellemaner@gmail.com>
2018-12-11 07:18:23 -0500

    Completed

--- index.js
+++ index.js
@@ -1,32 +1,114 @@
 $(document).ready(function(){
   $("#myDIV").hide();
-  $("#flip").hide();
-  $("#info").hide();
-
-  document.getElementById("answer").onclick = function() {myFunction()};
-
-  function myFunction() {
-      document.getElementById("answer").innerHTML = "True!";
-  };
-
-    document.getElementById("button").onclick = function() {newFunction()};
-
-  function newFunction() {
-    $("#myDIV").slideToggle();
-  };
-
-document.getElementById("more").onclick = function() {thirdFunction()};
-
-  function thirdFunction() {
-    $("#flip").show();
-  };
-
-    $("#flip").click(function(){
-        $("#info").slideToggle("slow");
-    });
+  $("#first").hide();
+  $("#answerone").hide();
+  $("#chart").hide();
+  $("#Questiontwo").hide();
+  $("#answertwo").hide();
+  $("#Questionthree").hide();
+  $("#answerthree").hide();
+  $("#three").hide();
+  $("#third").hide();
+  $("#final").hide();
 
 
 
+$(".questionstyle").click(function(){
+       $("#answerone").slideToggle();
+   });
+
+$("#down").click(function(){
+    $("#first").slideToggle("slow");
+});
+
+
+$("#right").click(function(){
+    $("#firstquestion").fadeOut("slow");
+    $("#chart").fadeIn();
+});
+
+
+$("#up").click(function(){
+  $("#chart").slideUp("slow");
+  $("#Questiontwo").show();
+});
+
+$("#next").click(function(){
+  $("#answertwo").slideToggle();
+});
+
+
+
+$("#dione").mouseenter(function(){
+  $("#dione").text("No");
+});
+
+$("#dione").mouseleave(function(){
+  $("#dione").text("Influenza");
+});
+
+$("#ditwo").mouseenter(function(){
+  $("#ditwo").text("No");
+});
+
+$("#ditwo").mouseleave(function(){
+  $("#ditwo").text("Asthma");
+});
+
+$("#dithree").mouseenter(function(){
+  $("#dithree").text("Yes");
+});
+
+$("#dithree").mouseleave(function(){
+  $("#dithree").text("STDs");
+});
+
+$("#difour").mouseenter(function(){
+  $("#difour").text("Yes");
+});
+
+$("#difour").mouseleave(function(){
+  $("#difour").text("Shingles");
+});
+$("#difive").mouseenter(function(){
+  $("#difive").text("No");
+});
+
+$("#difive").mouseleave(function(){
+  $("#difive").text("Chickenpox");
+});
+$("#disix").mouseenter(function(){
+  $("#disix").text("Yes");
+});
+
+$("#disix").mouseleave(function(){
+  $("#disix").text("Diabetes");
+});
+
+
+$("#righttwo").click(function(){
+    $("#secondquestion").fadeOut("slow");
+    $("#Questionthree").fadeIn();
+});
+
+
+
+
+
+
+
+$("#stylethree").click(function(){
+    $("#answerthree").slideToggle("slow");
+});
+
+$("#downtwo").click(function(){
+  $("#third").slideToggle();
+});
+
+$("#rightthree").click(function(){
+  $("#thirdquestion").fadeOut("slow");
+  $("#final").fadeIn();
+});
 
 
 
